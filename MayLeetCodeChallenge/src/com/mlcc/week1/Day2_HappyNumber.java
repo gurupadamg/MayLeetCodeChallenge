b75fe358364317a04b3f682eed1aa58300b21964
Gurupadam Gundala <gurupadamg@gmail.com>
2020-05-02 22:49:11 +0530

    Happy Number Solution

---
+++ MayLeetCodeChallenge/src/com/mlcc/week1/Day2_HappyNumber.java
@@ -0,0 +1,48 @@
+package com.mlcc.week1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Day2_HappyNumber {
+
+	/*
+	 * Write an algorithm to determine if a number n is "happy".
+	 * 
+	 * A happy number is a number defined by the following process: Starting with
+	 * any positive integer, replace the number by the sum of the squares of its
+	 * digits, and repeat the process until the number equals 1 (where it will
+	 * stay), or it loops endlessly in a cycle which does not include 1. Those
+	 * numbers for which this process ends in 1 are happy numbers.
+	 * 
+	 * Return True if n is a happy number, and False if not.
+	 * 
+	 * Example:
+	 * 
+	 * Input: 19 Output: true Explanation: 12 + 92 = 82 82 + 22 = 68 62 + 82 = 100
+	 * 12 + 02 + 02 = 1
+	 */
+	
+    public static boolean isHappy(int n) {
+        Set<Integer> dataSet = new HashSet<Integer>();
+        dataSet.add(n);
+        while(n != 1) {
+        	int sum = 0;
+        	while(n > 0) {
+        		int digit = n % 10;
+        		n = n / 10;
+        		sum = sum + (digit * digit);
+        	}
+        	n = sum;
+        	if(dataSet.contains(n))
+        		return false;
+        	dataSet.add(n);
+        }
+        
+        return true;
+    }
+    
+    public static void main(String[] args) {
+		System.out.println("Test Case1: " + isHappy(19));
+		System.out.println("Test Case1: " + isHappy(119));
+	}
+}
