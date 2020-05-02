7d7a565853af171a73cf1012a2a5d00982f8142d
Gurupadam Gundala <gurupadamg@gmail.com>
2020-05-02 12:33:15 +0530

    First Bad Version Commit - Day 1

--- MayLeetCodeChallenge/src/com/mlcc/week1/FirstBadVersion.java
+++ MayLeetCodeChallenge/src/com/mlcc/week1/FirstBadVersion.java
@@ -25,12 +25,26 @@ public class FirstBadVersion {
 	 * 
 	 * Then 4 is the first bad version.
 	 */
-	
+
 	public int firstBadVersion(int n) {
-		return -1;
+		int left = 1;
+		int right = n;
+
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+
+			if (isBadVersion(mid)) {
+				right = mid;
+			} else {
+				left = mid + 1;
+			}
+
+		}
+		return left;
 	}
 
 	private boolean isBadVersion(int i) {
 		return i > 10 ? true : false;
 	}
+
 }
